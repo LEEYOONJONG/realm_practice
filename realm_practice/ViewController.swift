b137f4a99169feaa238b8448285e3de96c0706fd
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-08-13 12:00:43 +0900

    Initial Commit

---
+++ realm_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  realm_practice
+//
+//  Created by YOONJONG on 2021/08/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
