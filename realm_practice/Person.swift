28af2643f3ad76f24a2bb2a27458a982b6849ab9
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-08-15 23:55:43 +0900

    210815

--- realm_practice/Person.swift
+++ realm_practice/Person.swift
@@ -5,6 +5,7 @@ class Person: Object{
     @Persisted(primaryKey: true) var _id : ObjectId
     @Persisted var name: String = ""
     @Persisted var age: Int?
+    
     convenience init(name: String) {
         self.init()
         self.name = name
